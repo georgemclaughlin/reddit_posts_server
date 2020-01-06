4751b57de0ce3c9142c52f9a7a35962acad5e7a3
Dillon Reedy <dillon.reedy123@gmail.com>
2020-01-05 22:04:51 -0600

    Created functionality to create a dictionary object

---
+++ src/helperFunctions/index.js
@@ -0,0 +1,16 @@
+var snoowrap = require('snoowrap');
+
+function createSnoowrap(credentials) {
+    return new snoowrap(credentials);
+}
+
+function titleBreakdown(title) {
+    var words = title.split(' ');
+    return words.map(word => word.replace(/[^a-zA-Z]/g, ""));
+}
+
+
+module.exports = {
+    createSnoowrap,
+    titleBreakdown
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 var cors = require('cors');
 var express = require('express');
+var helperFunctions = require('./helperFunctions/index');
+
 require('dotenv').config();
+
 
 const app = express();
 
@@ -13,8 +16,28 @@ app.get('/', (req, res) => {
 app.get('/GetRedditTitles', (req, res) => {
     if (!req.query.subreddit || req.query.subreddit === "") throw new Error("Cannot input empty subreddit name.");
 
-    // An example url input is: http://localhost:3001/GetRedditTitles?subreddit=all
-    res.send(`The subreddit you input in the url is: ${req.query.subreddit}`);
+    const sw = helperFunctions.createSnoowrap({
+        userAgent: process.env.USER_AGENT,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN
+      });
+
+
+    sw.getSubreddit(req.query.subreddit).getHot({limit: 100}).then(function(data) {
+        var results = {};        
+        data.forEach((element) => {
+            var words = helperFunctions.titleBreakdown(element.title);
+
+            words.forEach(word => {
+                if (results[word] === undefined) results[word] = 1;
+                else results[word]++;
+            })
+
+        });
+        res.send(results);
+    });
+
 });
 
 app.listen(process.env.PORT, () =>
